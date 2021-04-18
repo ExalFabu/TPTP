14c3b488215ad68df9fcc6a913ab895349de2182
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2021-04-18 11:49:08 +0200

    Added Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image, useColorModeValue } from '@chakra-ui/react';
+import logo_black from './logos/logofhd-b.png';
+import logo_white from './logos/logofhd-w.png';
+
+
+export const Logo = props => {
+  const logo_v = useColorModeValue(logo_black, logo_white)
+  return <Image src={logo_v} {...props}/>;
+};
