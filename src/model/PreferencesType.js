c56f0648eac93db222084bd32d7aea799a36bf7f
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2021-04-21 12:17:42 +0200

    Started adding some type definitions

---
+++ src/model/PreferencesType.js
@@ -0,0 +1,14 @@
+/**
+ Options
+ @typedef {Object} Preferences
+ @property {string} cfu_or_mat
+ @property {boolean} removeCFU
+ @property {number} cfu_value
+ @property {number} mat_value
+ @property {number} ptlode
+ @property {number} incorso
+ @property {number} erasmus
+ */
+
+
+export default Preferences
