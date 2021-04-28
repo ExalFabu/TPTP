a9b02441c52edf530289cfea668258d1a5092c0a
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2021-04-28 13:45:09 +0200

    Completed AverageTab component to a pleasant state

---
+++ src/model/FinalAverage.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} FinalAverage
+ * @property {boolean} isInCorso - Se la laurea è in corso
+ * @property {boolean} hasDoneEramus - Se ha fatto erasmus
+ * @property {number} averageBonus - Valutazione da sommare al voto finale in base alla media
+ */
+
+export default FinalAverage
