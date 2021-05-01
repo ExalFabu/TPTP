d6c790ffb9d0f93e096a5ffb15d5b025edd3d0d4
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2021-05-01 13:44:05 +0200

    Forgot warning

--- src/components/Average.js
+++ src/components/Average.js
@@ -10,7 +10,6 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverTrigger,
-  Tooltip,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { borderColor } from '../theme';
