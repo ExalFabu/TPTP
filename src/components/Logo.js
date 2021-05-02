e8790459576ba75804865d896d0fb3d841e4767c
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2021-05-02 14:33:54 +0200

    Updated Logo component
    Now it loads both pictures and hides the other

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,20 @@ import { Image, useColorModeValue } from '@chakra-ui/react';
 import logo_black from './logos/logofhd-b.png';
 import logo_white from './logos/logofhd-w.png';
 
+const WhiteLogo = ({ colorMode, ...props}) => {
+  return <Image alt="TPTP Logo" display={colorMode==="black" ? "inherit" : "none"} src={logo_white} {...props} />
+}
+
+const BlackLogo = ({colorMode, ...props}) => {
+  return <Image alt="TPTP Logo" display={colorMode==="white" ? "inherit" : "none"} src={logo_black} {...props} />
+}
 
 export const Logo = props => {
-  const logo_v = useColorModeValue(logo_black, logo_white)
-  return <Image alt="TPTP Logo" src={logo_v} {...props}/>;
+  const colorMode = useColorModeValue("white", "black");
+  return <>
+    <BlackLogo colorMode={colorMode} {...props}/>
+
+    <WhiteLogo colorMode={colorMode} {...props}/>
+
+  </>
 };
